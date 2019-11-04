9c225f8a4b8345100a2579bcb8e326d6cd06755b
annam <annamaria.vater@web.de>
2019-11-04 14:51:47 +0100

    google firebase in src_new angepasst

--- src_new/Database/database.js
+++ src_new/Database/database.js
@@ -56,79 +56,63 @@ class Database {
     async createDemoData() {
         let posts = await this.selectAllPosts();
 
-        /* DATENSTRUKTUR
-        *
-        *    "id": "1",
-        *    "title": "Reise nach Hamburg",
-        *    "authors": "Leonie",
-        *    "land": "Dautschland",
-        *    "Datum": "11.05.2013",
-        *    "content": "Es war eine schöne Reise.",
-        *    "picture": "img"
-        */
         if (posts.length < 1) {
             this.savePosts([{
                 "id": "1",
                 "title": "Reise nach Stockholm",
-                "authors": "Anna-Maria Vater",
+                "author": "Anna",
                 "land": "Schweden",
-                "Datum": "24.06.2012",
-                "content": "Hallo liebe Leute, die Reise nach Stockholm war atemberaubend! :) "
-                /** "picture:"https://upload.wikimedia.org/wikipedia/commons/c/c3/Stockholm.jpg" */
-            },{
+                "content": "Hallo liebe Leute, die Reise nach Stockholm war atemberaubend! :)",
+                "year": 2019,
+            }, {
                 "id": "2",
                 "title": "Roadtrip entlang der griechischen Küste",
-                "authors": "Henriette Wien",
+                "author": "Henriette ",
                 "land": "Griechenland",
-                "Datum": "12.10.2015",
-                "content": "Grüß euch Weltenbummler, in diesem Post stelle ich die schönsten Reiseziele entlang der grichischen Küste vor. "
-              /**  "picture:"https://upload.wikimedia.org/wikipedia/commons/6/6e/Ionian_sea_islands%2C_pic1.JPG" */
-            },{
+                "content": "Grüß euch Weltenbummler, in diesem Post stelle ich die schönsten Reiseziele entlang der griechischen Küste vor.",
+                "year": 2008,
+            }, {
                 "id": "3",
                 "title": "Wochenendausflug nach Paris",
-                "authors": "Leonie Neis",
+                "author": "Leonie",
                 "land": "Frankreich",
-                "Datum": "14.02.2008",
-                "content": "Hallo Freunde der Reiselust, heute berichte ich euch von meinem Wochenendausflug in die Stadt der Liebe. "
-                /** "picture:"https://upload.wikimedia.org/wikipedia/de/c/cf/Paris_Montage_Ancient_and_Modern.png" */
-            },{
+                "content": "Hallo Freunde der Reiselust, heute berichte ich euch von meinem Wochenendausflug in die Stadt der Liebe.",
+                "year": 2019,
+            }, {
                 "id": "4",
                 "title": "Kulinarische Genüsse in Rom",
-                "authors": "Leonie Neis",
+                "author": "Leonie",
                 "land": "Italien",
-                "Datum": "05.08.2016",
-                "content": "Ciao amici, kennt ihr schon die 10 besten Restaurants in ganz Rom?"
-              /**  "picture:"https://upload.wikimedia.org/wikipedia/commons/a/ae/Pasta-Herstellung.jpg" */
-            },{
+                "content": "Ciao amici, kennt ihr schon die 10 besten Restaurants in ganz Rom?",
+                "year": 2019,
+            }, {
                 "id": "5",
                 "title": "Roadtrip durch die USA",
-                "authors": "Henriette Wien",
+                "author": "Henriette",
                 "land": "Amerika",
-                "Datum": "15.09.2017",
-                "content": "Heey friends, der Roadtrip entlang der Route 66 war LIFECHANGING! "
-                /** "picture:"https://upload.wikimedia.org/wikipedia/commons/1/11/Route66_2004.jpg" */
-            },{
+                "content": "Heey friends, der Roadtrip entlang der Route 66 war LIFECHANGING!",
+                "year": 2019,
+            }, {
                 "id": "6",
-                "title": "Begegnung mit einer Geisha in Kyoto ",
-                "authors": "Anna-Maria Vater",
+                "title": "Begegnung mit einer Geisha in Kyoto",
+                "author": "Anna",
                 "land": "Japan",
-                "Datum": "25.11.2018",
-                "content": "Hallo Freunde, in diesem Post berichte ich über meine Erlebnnisse in der ehemaligen, japanischen Hauptstadt Kyoto."
-                /**"picture:"https://upload.wikimedia.org/wikipedia/commons/f/f0/150124_At_Yasakakamimachi_Kyoto_Japan01n.jpg */
+                "content": "Hallo Freunde, in diesem Post berichte ich über meine Erlebnnisse in der ehemaligen, japanischen Hauptstadt Kyoto.",
+                "year": 2019,
             }]);
         }
     }
     /**
-     * Gibt alle in der Datenbank gespeicherten Posts zurück. Hier gilt
+     * Gibt alle in der Datenbank gespeicherten Bücher zurück. Hier gilt
      * dasselbe wie im Kommentar zur Methode createDemoData() geschrieben.
      * Alle Dokumente auf einmal auszulesen ist nur dann eine gute Idee,
      * wenn man weiß, dass es nicht viele geben kann. Besser wäre daher,
      * die Menge mit der where()-Funktion von Firebase einzuschränken.
      *
-     * @returns Promise-Objekt mit den gespeicherten Posts
+     * @returns Promise-Objekt mit den gespeicherten Büchern
      */
     async selectAllPosts() {
-        let result = await this._posts.orderBy("id").get();
+        let result = await this._posts.orderBy("title").get();
         let posts = [];
 
         result.forEach(entry => {
@@ -144,35 +128,33 @@ class Database {
      * @param id: ID des gesuchten Buches
      * @returns Promise-Objekt mit dem gesuchten Buch
      */
-
     async selectPostById(id) {
         let result = await this._posts.doc(id).get();
         return result.data();
     }
 
     /**
-     * Speichert ein einzelnen Post in der Datenbank. Das hierfür übergebene
+     * Speichert ein einzelnes Buch in der Datenbank. Das hierfür übergebene
      * Objekt sollte folgenden Aufbau haben:
      *
      *      {
-     *          "id": "nummer",
-     *          "title": "Titel des Posts",
-     *          "authors": "Name des Autors",
-     *          "land": "Name des Landes",
-     *          "Datum": "Datum der Reise",
-     *          "content": "Text des Posts",
-     *          "picture": "img"                momentan noch auskommentiert!!!
+     *          id:        "MeinBuch1",
+     *          title:     "Name des Buches",
+     *          authors:   "Namen der Autoren",
+     *          edition:   "8. Auflage",
+     *          publisher: "Name des Verlags",
+     *          year:      2019,
      *      }
      *
-     * @param posts: Zu speicherndes Post-Objekt
+     * @param books: Zu speicherndes Buch-Objekt
      */
     savePost(post) {
         this._posts.doc(post.id).set(post);
     }
 
     /**
-     * Löscht ein einzelnen Post aus der Datenbank.
-     * @param id: ID des zu löschenden Posts
+     * Löscht ein einzelnes Buch aus der Datenbank.
+     * @param id: ID des zu löschenden Buches
      * @returns Promise-Objekt zum Abfangen von Fehlern oder Warten auf Erfolg
      */
     async deletePostById(id) {
@@ -180,18 +162,17 @@ class Database {
     }
 
     /**
-     * Speichert die übergebenen Posts in der Datenbank. Die hier übergebene
+     * Speichert die übergebenen Bücher in der Datenbank. Die hier übergebene
      * Liste sollte folgenden Aufbau haben:
      *
      *      [
      *          {
-     *              "id": "nummer",
-     *              "title": "Titel des Posts",
-     *              "authors": "Name des Autors",
-     *              "land": "Name des Landes",
-     *              "Datum": "Datum der Reise",
-     *              "content": "Text des Posts",
-     *              "picture": "img"                momentan noch auskommentiert!!!
+     *              id:        "MeinBuch1",
+     *              title:     "Name des Buches",
+     *              authors:   "Namen der Autoren",
+     *              edition:   "8. Auflage",
+     *              publisher: "Name des Verlags",
+     *              year:      2019,
      *          }, {
      *              ...
      *          },
@@ -212,11 +193,10 @@ class Database {
     }
 
     /**
-     * Löscht eines oder mehrerer Posts aus der Datenbank.
-     * @param ids: Liste der IDs der zu löschenden Posts
+     * Löscht eines oder mehrerer Bücher aus der Datenbank.
+     * @param ids: Liste der IDs der zu löschenden Bücher
      * @returns Promise-Objekt zum Abfangen von Fehlern oder Warten auf Erfolg
      */
-
     async deletePostsById(ids) {
         let batch = this._db.batch();
 
